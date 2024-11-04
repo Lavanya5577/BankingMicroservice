b00d0e2f154f5f90deb4c95b604fa92a9aac0559
Lavanya5577 <145743443+Lavanya5577@users.noreply.github.com>
2024-11-04 22:03:23 +0530

    Update App.java

--- src/main/java/com/lavanya/App.java
+++ src/main/java/com/lavanya/App.java
@@ -5,6 +5,6 @@ package com.lavanya;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Lavanya!");
+        System.out.println("Hello Chathu!");
     }
 }
